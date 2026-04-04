02b8ab843de72473b65ba1bff848ab47c4836e2a
unknown <mh.z77w@gmail.com>
2026-04-04 14:40:09 +0330

    feat: implement post submission

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,12 @@
+import PostCreate from "./PostCreate";
+
 const App = () => {
-  return <div>Blog app</div>;
+  return (
+    <div className="">
+      <h1>Create Post</h1>
+      <PostCreate />
+    </div>
+  );
 };
 
 export default App;
---
+++ client/src/PostCreate.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { useState } from "react";
+
+export default function PostCreate() {
+  const [title, setTitle] = useState("");
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    await axios.post("http://localhost:4000/posts", {
+      title,
+    });
+
+    setTitle("");
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className="">
+          <label>Title</label>
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className=""
+          />
+        </div>
+        <button className="">Submit</button>
+      </form>
+    </div>
+  );
+}
