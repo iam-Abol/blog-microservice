b657b5754a784e4b4effab5d76aa382d5362c282
unknown <mh.z77w@gmail.com>
2026-04-04 18:13:42 +0330

    feat: implement post list

---
+++ client/src/PostList.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export default function PostList(params) {
+  const [posts, setPosts] = useState({});
+  const fetchPosts = async () => {
+    const res = await axios.get("http://localhost:4000/posts");
+
+    setPosts(res.data);
+  };
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const renderedPosts = Object.values(posts).map((post) => {
+    return (
+      <div className="" key={post.id}>
+        <div>
+          <h3>{post.title}</h3>
+        </div>
+      </div>
+    );
+  });
+
+  return <div>{renderedPosts}</div>;
+}
