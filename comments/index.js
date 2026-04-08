7bd4aa3a08f88298f244483a772f684c46300dbb
unknown <mh.z77w@gmail.com>
2026-04-08 11:55:37 +0330

    feat(comments): emit CommentCreated event after creaeting a comment

--- comments/index.js
+++ comments/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { randomBytes } = require("crypto");
 const bodyParser = require("body-parser");
+const axios = require("axios");
 const cors = require("cors");
 const app = express();
 app.use(bodyParser.json());
@@ -11,12 +12,19 @@ app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
-  const commentid = randomBytes(4).toString("hex");
+app.post("/posts/:id/comments", async (req, res) => {
+  const commentId = randomBytes(4).toString("hex");
   const { content } = req.body;
   const comments = commentsByPostId[req.params.id] || [];
-  comments.push({ id: commentid, content });
+  comments.push({ id: commentId, content });
+
   commentsByPostId[req.params.id] = comments;
+
+  await axios.post("http://localhost:4005/events", {
+    type: "CommentCreated",
+    data: { id: commentId, content, postId: req.params.id },
+  });
+
   res.status(201).send(comments);
 });
 
