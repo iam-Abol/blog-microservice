ecd7cf3d449ffb5c178c15f2541f43dbedb3373a
unknown <mh.z77w@gmail.com>
2026-04-09 20:35:10 +0330

    feat(moderation): handle CommentCreated event

--- moderation/index.js
+++ moderation/index.js
@@ -4,11 +4,22 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
+app.post("/events", async (req, res) => {
+  const { type, data } = req.body;
+  if (type === "CommentCreated") {
+    const status = data.content.includes("orange") ? "rejected" : "approved";
 
-
-
-app.post("/events", (req, res) => {
-  
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentModerated",
+      data: {
+        id: data.id,
+        content: data.content,
+        postId: data.postId,
+        status,
+      },
+    });
+  }
+  res.send({});
 });
 
 app.listen(4003, () => {
